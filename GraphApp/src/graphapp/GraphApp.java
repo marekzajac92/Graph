60bfb33bcc3440285bf27e51193ba12a2f8122c5
Marek Z <marekzajac92@gmail.com>
2013-03-04 13:18:49 +0100

    Fragment implementacji menu

--- GraphApp/src/graphapp/GraphApp.java
+++ GraphApp/src/graphapp/GraphApp.java
@@ -8,15 +8,18 @@ import java.io.File;
  * @author Marek
  */
 public class GraphApp {
-
+    private static ScriptParser scriptParser;
+    private static Writer writer;
+    private static Graph[] graphs;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ScriptParser scriptP = new ScriptParser();
-        Graph[] grafy = scriptP.parse(new TextFileReader(new File("graf.gs")));
-        System.out.println("Zapis...");
-        TextFileWriter writer = new TextFileWriter("drugi.gs");
-        writer.writeGraphs(grafy);
+        boolean quit = false;
+        
+        while(!quit){
+            
+        }
     }
 }
